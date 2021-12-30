bf7febb6aaef6c5806ca536eb78a9ce8c37402ac
onshore <onshorecoding9@gmail.com>
2021-12-30 23:15:42 +0900

    Feat: 반응형적용 미디어쿼리 추가

--- portfolio/portfolio/src/components/Main/CategoryList.tsx
+++ portfolio/portfolio/src/components/Main/CategoryList.tsx
@@ -29,7 +29,12 @@ const CategoryListWrapper = styled.div`
   width: 768px;
   margin: 100px auto 0;
 
-  `
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 50px;
+    padding: 0 20px;
+  }
+`
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
@@ -42,9 +47,13 @@ const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
     cursor: pointer;
   
     &:last-of-type {
-      margin-right: 0;
-    }
-  `
+        margin-right: 0;
+      }
+    
+      @media (max-width: 768px) {
+        font-size: 15px;
+      }
+`
 
 const CategoryList: FunctionComponent<CategoryListProps> = function ({
     selectedCategory,
--- portfolio/portfolio/src/components/Main/ProfileImage.tsx
+++ portfolio/portfolio/src/components/Main/ProfileImage.tsx
@@ -3,17 +3,21 @@ import styled from '@emotion/styled'
 
 // 자신이 원하는 프로필 이미지 링크로 설정해주세요.
 const PROFILE_IMAGE_LINK =
-    'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif'
+  'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif'
 
 const ProfileImageWrapper = styled.img`
-  width: 120px;
-  height: 120px;
-  margin-bottom: 30px;
-  border-radius: 50%;
-`
-
+    width: 120px;
+    height: 120px;
+    margin-bottom: 30px;
+    border-radius: 50%;
+  
+    @media (max-width: 768px) {
+      width: 80px;
+      height: 80px;
+    }
+  `
 const ProfileImage: FunctionComponent = () => {
-    return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
 }
 
 export default ProfileImage
