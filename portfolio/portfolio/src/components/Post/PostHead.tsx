44a9a9a3b09b3b251ea5cb2ee1a9bbc9deb7a339
onshore <onshorecoding9@gmail.com>
2022-01-05 22:59:22 +0900

    Feat: 반응형 페이지 구현

--- portfolio/portfolio/src/components/Post/PostHead.tsx
+++ portfolio/portfolio/src/components/Post/PostHead.tsx
@@ -16,8 +16,11 @@ const PostHeadWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
-`
 
+  @media (max-width: 768px) {
+    height: 300px;
+  }
+`
 const BackgroundImage = styled((props: GatsbyImgProps) => (
     <GatsbyImage {...props} style={{ position: 'absolute' }} />
 ))`
