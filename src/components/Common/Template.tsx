a082ad0328f5545b6ef7ade7c6a237f1155674b9
onshore <onshorecoding9@gmail.com>
2022-03-05 22:28:31 +0900

    Feat: 구글 메타태그 추가

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -49,7 +49,7 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="twitter:image" content={image} />
         <meta name="twitter:site" content="@사용자이름" />
         <meta name="twitter:creator" content="@사용자이름" />
-        <meta name="google-site-verification" content="-uc8sVG2H02k6gAmsPRX4pvkgBa_n13N-EXE4L0ogm8" />
+        <meta name="google-site-verification" content="sTEv8p8rRAC40iCYXLmofvMaap3ja1Len0risu_7C_c" />
         <meta name="naver-site-verification" content="b8e4c399a5f5a8c51ae2657d933b5b3620c73296" />
       </Helmet>
       <Navigation />
